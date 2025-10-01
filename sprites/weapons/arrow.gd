2813cd02414fad5211b5fca5ba48cf9bc4596d96
clemenceroumy <clemenceroumy@gmail.com>
2025-10-01 15:09:55 +0200

    refactor: wip: shoot arrows from bow

--- sprites/weapons/arrow.gd
+++ sprites/weapons/arrow.gd
@@ -1,15 +1,11 @@
 extends Area2D
 
 var travelled_distance = 0
-const SPEED = 500
-const RANGE = 1200
+const SPEED = 200
+var RANGE = 1200
 
 func _physics_process(delta: float) -> void:
-	var mouse_position = get_viewport().get_mouse_position()
-	var direction = Vector2.RIGHT.rotated(mouse_position.angle())
-	
-	look_at(direction) #rotate sprite to look toward mouse
-	position += direction * delta * SPEED
+	position += Vector2.RIGHT.rotated(rotation) * delta * SPEED
 	travelled_distance = delta * SPEED
 	
 	if(travelled_distance > RANGE):
--- sprites/weapons/bow.gd
+++ sprites/weapons/bow.gd
@@ -1,10 +1,18 @@
-extends Marker2D
-	
+extends Area2D
+
+#AREA2D allow us to detect enemies in its surface. In my case, I don't really need it
+		
 func shoot(): 
 	const arrow = preload("res://sprites/weapons/arrow.tscn")
 	var new_arrow = arrow.instantiate()
-	#new_arrow.global_position = self.global_position
-	self.add_child(new_arrow)
+	
+	new_arrow.global_position = %ShootingPoint.global_position
+	new_arrow.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_arrow)
 	
 func _on_timer_timeout() -> void:
 	shoot()
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		$BoxPivot.look_at(event.position)
