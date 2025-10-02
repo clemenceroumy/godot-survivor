58b958cd205bbc7f1586350615bdd69d6d59b568
clemenceroumy <clemenceroumy@gmail.com>
2025-10-02 14:47:41 +0200

    feat: healthbar on orcs + hurt animation

--- sprites/characters/orc.gd
+++ sprites/characters/orc.gd
@@ -2,16 +2,32 @@ extends CharacterBody2D
 
 @onready var soldier = get_node('/root/Game/Soldier')
 
-func _physics_process(delta: float):
-	var direction = global_position.direction_to(soldier.global_position)
-	velocity = direction * 200
-	#move_and_slide()
+var is_walking: bool = true
+
+func _physics_process(delta: float):	
+	if(is_walking):
+		var direction = global_position.direction_to(soldier.global_position)
+		velocity = direction * 200
+		move_and_slide()
+		
+		if(velocity == Vector2.ZERO):
+			$AnimatedSprite2D.play("default")
+		else:
+			$AnimatedSprite2D.play("walk")
+			if(direction.x < 0):
+				$AnimatedSprite2D.flip_h = true
+			elif(direction.x > 0) :
+				$AnimatedSprite2D.flip_h = false
+
+func take_damage() -> void:
+	$HealthBar.value -= 1
+	is_walking = false
+	$AnimatedSprite2D.play('hurt')
 	
-	if(velocity == Vector2.ZERO):
-		$AnimatedSprite2D.play("default")
-	else:
-		$AnimatedSprite2D.play("walk")
-		if(direction.x < 0):
-			$AnimatedSprite2D.flip_h = true
-		elif(direction.x > 0) :
-			$AnimatedSprite2D.flip_h = false
+	if($HealthBar.value == 0):
+		queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if($AnimatedSprite2D.animation == 'hurt'):
+		is_walking = true
--- sprites/weapons/arrow.gd
+++ sprites/weapons/arrow.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var travelled_distance = 0
-const SPEED = 200
+const SPEED = 600
 var RANGE = 1200
 
 func _physics_process(delta: float) -> void:
@@ -16,3 +16,5 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
+	if(body.has_method("take_damage")):
+		body.take_damage()
