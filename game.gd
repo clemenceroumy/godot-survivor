8e795369ea0b4e7f7afb5811a80c9f0a55effe28
clemenceroumy <clemenceroumy@gmail.com>
2025-10-02 14:59:15 +0200

    feat: add random spawn orcs

--- game.gd
+++ game.gd
@@ -4,3 +4,12 @@ var crosshair = load("res://assets/sprites/crosshair.png")
 
 func _process(delta: float) -> void:
 	Input.set_custom_mouse_cursor(crosshair)
+
+func spawn_orc():
+	%PathFollow2D.progress_ratio = randf()
+	var new_orc = preload("res://sprites/characters/orc.tscn").instantiate()
+	new_orc.global_position = %PathFollow2D.global_position
+	add_child(new_orc)
+
+func _on_timer_timeout() -> void:
+	spawn_orc()
